1e27e588fce2853138e39b0905f77f4b3988a716
Slamet <slamet@pon.co.id>
2024-12-12 13:03:32 +0700

    update navbar - add text

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,13 @@
 export default function Navbar() {
   const navbar = document.createElement('nav');
   navbar.className =
-    'bg-blue-600 text-white py-2 shadow-md fixed top-0 left-0 w-full z-10';
+    'bg-blue-600 text-white shadow-md fixed top-0 left-0 w-full z-10';
 
   navbar.innerHTML = `
     <div class="container px-3 flex justify-between items-center">
-      <a href="/SmartDoseLink">
+      <a href="/SmartDoseLink" class="rounded-xl flex flex-row">
         <img src="/SmartDoseLink/public/images/logoSmartDoseLink50x50.webp" alt="SmartDoseLink">
+        <div class="px-3 font-sans text-sm font-thin text-white">Smart<span class="font-bold">Dose</span>Link</div>
       </a>
       <ul class="flex space-x-4">
         <li><a href="/SmartDoseLink/pages/dashboard" class="hover:text-gray-200">Dashboard</a></li>
