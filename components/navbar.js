9db9429d9ef95ca44fe5412db3b5a460076ee9c8
Slamet <slamet@pon.co.id>
2024-12-12 08:01:12 +0700

    design page-routing

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,17 @@
 export default function Navbar() {
   const navbar = document.createElement('nav');
   navbar.className =
-    'bg-blue-600 text-white p-4 shadow-md fixed top-0 left-0 w-full z-10';
+    'bg-blue-600 text-white py-4 px-3 shadow-md fixed top-0 left-0 w-full z-10';
 
   navbar.innerHTML = `
     <div class="container mx-auto flex justify-between items-center">
-      <h1 class="text-xl font-bold">SmartDoseLink</h1>
+      <a href="/">
+        <img src="../public/images/private51x51.png" alt="SmartDoseLink">
+      </a>
       <ul class="flex space-x-4">
-        <li><a href="#" class="hover:text-gray-200">Home</a></li>
-        <li><a href="#" class="hover:text-gray-200">About</a></li>
-        <li><a href="#" class="hover:text-gray-200">Contact</a></li>
+        <li><a href="/pages/dashboard" class="hover:text-gray-200">Dashboard</a></li>
+        <li><a href="/pages/config" class="hover:text-gray-200">Config</a></li>
+        <li><a href="/pages/about" class="hover:text-gray-200">About</a></li>
       </ul>
     </div>
   `;
